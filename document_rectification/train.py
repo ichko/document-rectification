1e6a1b00bb35d72e0f4f27d22fcd8f585140f412
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-06-08 00:47:47 +0300

    Add logging

--- document_rectification/train.py
+++ document_rectification/train.py
@@ -6,6 +6,7 @@ import pytorch_lightning as pl
 import torch
 import torchvision
 from ez_torch.models import SpatialUVOffsetTransformer
+from pytorch_lightning import loggers
 from torch.nn import functional as F
 
 from document_rectification import data
@@ -30,26 +31,41 @@ class GeometricTransformModel(pl.LightningModule):
 
     def criterion(self, y_hat, y):
         y = y.mean(dim=1, keepdim=True)
-        assert y.shape == y_hat.shape
         return F.binary_cross_entropy(y_hat, y)
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=1e-8)
 
     def training_step(self, batch, _batch_index):
         x, y = batch["x"], batch["y"]
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
+
+        self.log("loss", loss)
+
         return loss
 
 
 def main():
     DEVICE = "cuda"
+    hparams = {}
+
+    tb_logger = loggers.TensorBoardLogger(
+        save_dir=".logs",
+        name=".checkpoints",
+    )
+    # tb_logger.log_hyperparams(hparams)
+
     model = GeometricTransformModel(res_w=20, res_h=20)
     model = model.to(DEVICE)
     datamodule = data.get_datamodule(train_bs=16, val_bs=16, plot_bs=8)
 
-    trainer = pl.Trainer(gpus=1)
+    trainer = pl.Trainer(
+        gpus=1,
+        logger=[tb_logger],
+        log_every_n_steps=1,
+        flush_logs_every_n_steps=3,
+    )
     trainer.fit(model, datamodule=datamodule)
 
 
