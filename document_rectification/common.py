da9ba16785d3c6f4d3e9788746d6d93e17483a11
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-06-12 00:11:59 +0300

    Add src to logger construction

--- document_rectification/common.py
+++ document_rectification/common.py
@@ -3,6 +3,7 @@ import sys
 
 
 def get_logger():
+    # SRC - https://stackoverflow.com/questions/533048/how-to-log-source-file-name-and-line-number-in-python
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler(sys.stdout)
