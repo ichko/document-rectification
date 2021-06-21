c3761fbbb76f19140f02e2532c0624074ace772b
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-06-22 00:31:04 +0300

    Add ae training

--- document_rectification/models/auto_encoder.py
+++ document_rectification/models/auto_encoder.py
@@ -77,10 +77,48 @@ class AutoEncoder(pl.LightningModule):
     def criterion(self, y_hat, y):
         return F.binary_cross_entropy(y_hat, y)
 
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
 
-def sanity_check():
+    def training_step(self, batch, _batch_id):
+        self.train()
+        x, y = batch["x"], batch["y"]
+
+        y_hat = self(y)
+
+        y = y.mean(dim=1)
+        y_hat = y_hat.mean(dim=1)
+
+        loss = self.criterion(y_hat, y)
+        self.log("loss", loss)
+        return loss
+
+    def on_epoch_start(self):
+        self.eval()
+        with torch.no_grad():
+            if self.plot_dataloader is not None:
+                for batch in self.plot_dataloader:
+                    x, y = batch["x"], batch["y"]
+                    y_hat = self(y)
+
+                    fig = Fig(nr=1, nc=2, figsize=(15, 10))
+
+                    im = y.ez.grid(nr=4).channel_last.np
+                    fig[0].imshow(im)
+                    fig[0].ax.set_title("Input")
+
+                    im = y_hat.ez.grid(nr=4).channel_last.np
+                    fig[1].imshow(im)
+                    fig[1].ax.set_title("Pred")
+
+                    plt.tight_layout()
+                    wandb.log({"chart": plt})
+                    plt.close()
+
+
+def main():
     dm = DocumentsDataModule(
-        train_bs=12,
+        train_bs=32,
         val_bs=32,
         plot_bs=16,
         shuffle=False,
@@ -90,19 +128,50 @@ def sanity_check():
     dl = dm.plot_dataloader()
     batch = next(iter(dl))
 
-    ae = AutoEncoder(
+    model = AutoEncoder(
         image_channels=3,
         latent_size=image_size[0] * image_size[1],
         decoder_initial_reshape=image_size,
-    ).to(DEVICE)
-    ae.summarize()
+        plot_dataloader=dm.plot_dataloader(),
+    )
+    model.summarize()
 
-    y_hat = ae.encoder(batch["x"])
-    x_hat = ae.decoder(y_hat)
+    hparams = Namespace(
+        lr=1e-4,
+    )
 
-    print(x_hat.shape)
-    print(y_hat.shape)
+    logger = loggers.WandbLogger(
+        save_dir=".logs",
+        project="document-rectification-ae",
+    )
+    logger.log_hyperparams(hparams)
+
+    dm = DocumentsDataModule(
+        train_bs=6,
+        val_bs=8,
+        plot_bs=8,
+        shuffle=True,
+        device=DEVICE,
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="loss",
+        dirpath=".checkpoints/",
+        filename="ae-{epoch:02d}-{loss:.2f}",
+        save_top_k=5,
+        mode="min",
+    )
+
+    trainer = pl.Trainer(
+        gpus=1 if DEVICE == "cuda" else None,
+        callbacks=[checkpoint_callback],
+        logger=[logger],
+        log_every_n_steps=1,
+        flush_logs_every_n_steps=3,
+        max_epochs=150_000,
+    )
+    trainer.fit(model, datamodule=dm)
 
 
 if __name__ == "__main__":
-    sanity_check()
+    main()
